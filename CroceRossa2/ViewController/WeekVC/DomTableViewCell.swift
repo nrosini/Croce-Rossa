d520a068431648535b0dc1740892321477388c8c
Nicolò Rosini <nicolo@iMac-di-Nicolo.local>
2018-09-04 12:27:41 +0200

    Added some features and fixed some problems on the medicines add form

---
+++ CroceRossa2/ViewController/WeekVC/DomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DomTableViewCell.swift
+//  CroceRossa2
+//
+//  Created by Nicolò Rosini on 04/09/18.
+//  Copyright © 2018 Informatica_4Binf. All rights reserved.
+//
+
+import UIKit
+
+class DomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
