6fbd815c295bb2057753b490c7fe799a51b77bf8
Nicolò Rosini <nicolo@iMac-di-Nicolo.local>
2018-09-04 15:43:30 +0200

    Finished the form for the medicine add

---
+++ CroceRossa2/ViewController/CelleCV/LunCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LunCollectionViewCell.swift
+//  CroceRossa2
+//
+//  Created by Nicolò Rosini on 04/09/18.
+//  Copyright © 2018 Informatica_4Binf. All rights reserved.
+//
+
+import UIKit
+
+class LunCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
